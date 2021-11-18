e451530469e663e63dd671c97dd7b45109c00159
Vivekanandan <41379292+vivekpatiz@users.noreply.github.com>
2021-11-19 00:31:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
+const shortid = require('shortid')
+const bodyParser = require('body-parser')
+const rndm = require('random-simple')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(process.env.PORT);
+
+let users = []
+let urls = []
+let shorturl;
+app.post("/createuser", function (req, res) {
+    users.push(req.body)
+})
+
+app.get("/login", function (req, res) {
+    res.json(users);
+})
+
+app.get("/user/:id", function (req, res) {
+    let username = req.params.id;
+    console.log(username)
+    res.json(username)
+})
+app.get('/language/:id', function (req, res) {
+  var val = {
+    "id": 21797,
+    "username": "vivekanandanrk",
+    "fullname": "Vivekanandan R K",
+    "firstname": "Vivekanandan",
+    "middlename": null,
+    "lastname": "R K",
+    "address1": "",
+    "city": "",
+    "state": "",
+    "postalcode": "",
+    "country": null,
+    "work": "",
+    "workext": null,
+    "email": "rk.vivekanandan@tcs.com",
+    "status": 0,
+    "dateformat": "MM-DD-YYYY",
+    "timeformat": "24",
+    "language": "en",
+    "defaultchartrange": 2,
+    "attachedsignature": 0,
+    "signature": "",
+    "avator": null
+  }
+  res.send(val)
+})
+app.post("/url", function (req, res) {
+
+    var short = require('node-url-shortener');
+
+    short.short(req.body, function (err, url) {
+        shorturl = url
+        console.log(shorturl)
+        res.json(url)
+    });
+
+})
+const tokenGenerator = require('generate-token');
+app.get("/random", function (req, res) {
+    let token = tokenGenerator.generate(5);
+    console.log(token);
+    res.json(token)
+})
